93cd549271006b75f4ed5362011bc9de50a69199
Lokeshch420 <lokeshcha2002@gmail.com>
2024-05-18 02:21:40 +0530

    creating a signup route and signup authentication and hashing the password

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express"
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import router from "./routes/user.routes.js"
+import userRouter from "./routes/user.routes.js"
+import authRouter from "./routes/auth.routes.js"
 
 dotenv.config()
 
@@ -14,7 +15,12 @@ mongoose.connect(process.env.MONGO_DB).then((msg)=>{
 
 const app = express()
 
-app.use('/api/user',router)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/user',userRouter)
+app.use('/api/auth', authRouter )
+
 
 app.listen(3000,()=>{
     console.log('app is listening at 3000!')
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -3,9 +3,9 @@ import User from "../models/user.models.js";
 import express from "express"
 
 
-const router = express.Router()
+const userRouter = express.Router()
 
 
-router.get("/test",test)
+userRouter.get("/test",test)
 
-export default router
+export default userRouter
