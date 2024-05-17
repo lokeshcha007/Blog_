472952a430ee299a419a209bb659438fb1c3a7d1
Lokeshch420 <lokeshcha2002@gmail.com>
2024-05-18 02:32:13 +0530

    created a middleware to handle the errors

--- api/index.js
+++ api/index.js
@@ -18,12 +18,22 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/user',userRouter)
-app.use('/api/auth', authRouter )
-
 
 app.listen(3000,()=>{
     console.log('app is listening at 3000!')
 })
 
+app.use('/api/user',userRouter)
+app.use('/api/auth', authRouter )
 
+
+
+app.use((err,req,res,next)=>{
+    const statuscode = err.statuscode || 500
+    const message = err.message || 'Internal Server Error'
+    res.status(statuscode).json({
+        success:false,
+        statuscode,
+        message
+    })
+})
